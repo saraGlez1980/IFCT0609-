8ad30c55a8c7b0a084c980d3719671359ab8787d
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-02-03 13:48:33 +0100

    ejercicio 6 arrays

--- programacion en C/solucionesEjerciciosArraysYpunteros/solucionEjercicio6.c
+++ programacion en C/solucionesEjerciciosArraysYpunteros/solucionEjercicio6.c
@@ -8,7 +8,9 @@ int main(void)
     for (i=0;i<11;i+=2) year[i]=31;
     year[1] = 28;
     for (i=3;i<10;i+=2) year[i]=30;
+    
     puntero=year;
+
     for (i=0;i<11;i++)
     {
         printf("%d",*puntero);
